69859034f4cd3a0d6ef5e12791e2c7fd98cc5da7
Brock Young <bearfoxfish@gmail.com>
2023-07-10 21:04:05 -0700

    add hover effect

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const gameboardContainer = document.querySelector(".gameboard-container");
 
 
 const Gameboard = (function() {
     let gameboard = [];
     const createBoard = () => {
+        const gameboardContainer = document.querySelector(".gameboard-container");
         const gameSquare = (x_coord, y_coord) => {
             let x = x_coord;
             let y = y_coord;
@@ -15,6 +15,24 @@ const Gameboard = (function() {
             for (let j =0; j<3; j++) {
                 let square = gameSquare(j, i);
                 gameboard.push(square);
+                const newSquare = document.createElement('div');
+                newSquare.classList.add('square');
+                newSquare.dataset.x= square.x;
+                newSquare.dataset.y= square.y;
+                gameboardContainer.appendChild(newSquare);
+                newSquare.addEventListener('mouseover', () => {
+                    if (square.symbol == "none") {
+                        square.symbol = "hover";
+                        const symbol = document.createElement('img');
+                        symbol.classList.add("x-img");
+                        symbol.src = "images/x.png";
+                        newSquare.appendChild(symbol);
+                    }
+                })
+                newSquare.addEventListener('mouseleave', () => {
+                    square.symbol = "none";
+                    newSquare.removeChild(newSquare.firstChild);
+                })
             }
         }
 
@@ -22,11 +40,7 @@ const Gameboard = (function() {
 
     const updateBoard = () => {
         for (square of gameboard) {
-            newSquare = document.createElement('div');
-            newSquare.classList.add('.square');
-            newSquare.dataset.x= square.x;
-            newSquare.dataset.y= square.y;
-            gameboardContainer.appendChild(newSquare);
+
         }
     }
     return {gameboard, createBoard, updateBoard};
